33b10308115fdbff4e00c40fb19651e7d075a252
grigaitisxunim <junior.grigaitis98@hotmail.com>
2021-05-12 17:25:16 -0300

    first commit

---
+++ projetoTelas/projetoTelas/src/teste.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class teste {
+    private JPanel panel1;
+    private JSpinner spinner1;
+    private JComboBox comboBox1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
